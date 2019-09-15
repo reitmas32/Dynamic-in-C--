37a2249e1bd7433c043431e3088470000876a31c
reitmas325 <42395690+reitmas325@users.noreply.github.com>
2019-09-14 20:05:32 -0500

    CReacion de la Clase Dynamic

--- Headers/include/Dynamic.hpp
+++ Headers/include/Dynamic.hpp
@@ -1,15 +1,36 @@
 #ifndef DYNAMIC_HPP
 #define DYNAMIC_HPP
 
-#include <string.h>
-#include "String.h"
+#include <string>
+
+using namespace std;
 
 class Dynamic{
     private:
         void* data;
-        string s;
+        string type;
     public:
         Dynamic();
+        Dynamic(void* dynamic);
+        Dynamic(bool dynamic);
+        Dynamic(char dynamic);
+        Dynamic(int dynamic);
+        Dynamic(size_t dynamic);
+        Dynamic(double dynamic);
+        Dynamic(float dynamic);
+
+        string getType();
+
+        void setData(void* dynamic);
+        void setData(bool dynamic);
+        void setData(char dynamic);
+        void setData(int dynamic);
+        void setData(size_t dynamic);
+        void setData(double dynamic);
+        void setData(float dynamic);
+
+
+        ~Dynamic();
 };
 
 #endif
--- Sources/src/main.cpp
+++ Sources/src/main.cpp
@@ -1,12 +1,53 @@
 #include <iostream>
 
-#include "../../Headers/include/String.hpp"
+#include <string>
 
 using namespace std;
 
+class Dynamic{
+    void* data;
+    public:
+        void operator=(const float s){
+            data=(void*)&s;
+        }
+        void operator=(const int p){
+            data=(void*)&p;
+        }
+
+        void operator=(const string p){
+            data=(void*)&p;
+        }
+
+        int getInt(){
+            return (*(int*)data);
+        }
+        float getFloat(){
+            return (*(float*)data);
+        }
+
+        string getString(){
+            return (*(string*)data);
+        }
+
+};
+
+
 int main(int argc, char const *argv[])
 {
-    StringPtr palabra = new_String("Hola Mundo\n");
-    cout<<Str_get(palabra)<<endl;
+    Dynamic m;
+
+    m = 5;
+
+    cout<<m.getInt()<<endl;
+
+    m = 4.8f;
+
+    cout<<m.getFloat()<<endl;
+
+
+    m = "jajajaja";
+    
+    cout<<m.getString()<<endl;
+
     return 0;
 }
